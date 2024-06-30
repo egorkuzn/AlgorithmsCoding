1becedf9f3d48435fd843dd7c2e372c39e345286
egorkuzn2002 <georkuzn2002@mail.ru>
2024-06-30 12:30:19 +0700

    Refactored binary search.

--- src/main/kotlin/common/binary_search/BinarySearch.kt
+++ src/main/kotlin/common/binary_search/BinarySearch.kt
@@ -1,8 +1,7 @@
 package common.binary_search
 
 fun binarySearch() {
-    val inputArray = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
-
+    val inputArray = arrayOf(1, 2, 3, 4, 5, 5, 7, 7, 9, 10, 11, 12, 13, 14, 15)
     val searchedValue = 7
     val index = search(inputArray, searchedValue)
 
@@ -12,12 +11,10 @@ fun binarySearch() {
 fun search(inputArray: Array<Int>, searchedValue: Int): Int {
     val size = inputArray.size
 
-    return if (inputArray.isEmpty())
-        throw Exception("Нет такого элемента")
-    else if (searchedValue == inputArray[size/2])
-        size / 2
-    else if (searchedValue in inputArray[size/2] .. inputArray.last())
-        size / 2 + search(inputArray.copyOfRange(size/2, size), searchedValue)
-    else
-        search(inputArray.copyOfRange(0, size/2), searchedValue)
+    return when {
+        inputArray.isEmpty() -> throw Exception("Нет такого элемента")
+        searchedValue == inputArray[size/2] -> size / 2
+        searchedValue in inputArray[size/2] .. inputArray.last() -> size / 2 + search(inputArray.copyOfRange(size / 2, size), searchedValue)
+        else -> search(inputArray.copyOfRange(0, size / 2), searchedValue)
+    }
 }
